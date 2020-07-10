c88e60f681b124ab0329cd71447eef1051059468
Saravana Kumar <saravk78@gmail.com>
2020-07-10 22:19:45 +0530

    post meta labels

--- template-parts/post-meta.php
+++ template-parts/post-meta.php
@@ -9,15 +9,26 @@
 <div class="post-meta-section">        
   By <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author"><?php the_author_meta( 'nickname', get_the_author_meta('ID') ); ?></a> | <?php echo get_the_date(); ?>
 <?php 
-  if (get_post_meta($post->ID, 'featured-checkbox', true) == 'yes') {
+  $postfeatured = (get_post_meta($post->ID, 'featured-checkbox', true) == 'yes') ? true : false;
+
+  //Get the age of the post
+  $ageunix = get_the_time('U');
+  $days_old_in_seconds = ((time() - $ageunix));
+  $days_old = (($days_old_in_seconds/86400));
+  $postnew = ($days_old < 10) ? true : false;
+
+  if ($postfeatured || $postnew) {
+?>
+    | 
+<?php        
+  }
+  if ($postfeatured) {
 ?>
     <span class="label label-featured">Featured</span>
 <?php 
   }
-  $ageunix = get_the_time('U');
-  $days_old_in_seconds = ((time() - $ageunix));
-  $days_old = (($days_old_in_seconds/86400));
-  if ($days_old < 10) {
+
+  if ($postnew) {
 ?>                
     <span class="label label-new">New</span>
 <?php
