724f976d1c9a63e6afe6adb8113ddc20a91b25b2
Saravana Kumar <saravk78@gmail.com>
2020-07-12 00:43:51 +0530

    added scrolltop

--- template-parts/header-scrolltop.php
+++ template-parts/header-scrolltop.php
@@ -17,7 +17,6 @@
     </script>
   </amp-animation>  
 <style amp-custom>
-
   .scrollToTop {
     color: var(--color-text-light);
     font-size: 1.4em;
@@ -35,5 +34,13 @@
     opacity: 0;
     visibility: hidden;
   }
+.scrollarrow {position: absolute;top: 50%;left: 50%;width: 18px;height: 18px;margin-left: -9px;margin-top: -5px;cursor: pointer;}
+.scrollarrow:hover polyline, .scrollarrow:focus polyline {stroke-width: 15;}
+.scrollarrow:active polyline {stroke-width: 15;transition: all 100ms ease-in-out;}  
 </style>
-<button id="scrollToTopButton" on="tap:siteheader.scrollTo(duration=200)" class="scrollToTop">⌃</button>  
+<button id="scrollToTopButton" on="tap:siteheader.scrollTo(duration=200)" class="scrollToTop">
+  <svg class="scrollarrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="5 0 50 80" xml:space="preserve">
+    <polyline fill="none" stroke="#FFFFFF" stroke-width="8" stroke-linecap="round" stroke-linejoin="round" points="
+0.375, 35.375 28.375, 0.375 58.67, 35.375 " />
+  </svg>
+</button>  
